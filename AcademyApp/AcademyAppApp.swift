5c137e42a418164810e27a2c31e8cb5bbc23ccad
Yuri Elias <yuririelias15@gmail.com>
2025-03-12 16:51:30 -0300

    Initial Commit

---
+++ AcademyApp/AcademyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcademyAppApp.swift
+//  AcademyApp
+//
+//  Created by User on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AcademyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

df5ac22362d795b0521c8c1e1263e5cb3670c481
Yuri Elias <yuririelias15@gmail.com>
2025-03-13 14:14:41 -0300

    Update TaskView

--- AcademyApp/TaskView.swift
+++ AcademyApp/TaskView.swift
@@ -8,18 +8,43 @@
 import SwiftUI
 
 struct TaskView: View {
-    let task: TaskModel
+    var task: TaskModel
     
     var body: some View {
-        HStack{
-            VStack {
-                Text("Titulo")
-                Text("Data")
+        HStack {
+            VStack(alignment: .leading) {
+                Text(task.title)
+                Text(task.date.formatted())
+                    .foregroundStyle(.secondary)
             }
+            
+            Spacer()
+            
+            Button {
+                task.isDone.toggle()
+            } label: {
+                Label {
+                    Text("checkbox")
+                } icon: {
+                    Image(systemName: task.isDone
+                          ? "checkmark.square.fill"
+                          : "square")
+                }
+                .labelStyle(.iconOnly)
+
+            }
+
         }
-}
+        
+    }
 }
 
 #Preview {
-    TaskView(task: TaskModel(title: "Teste", date: Date, isDone: Bool))
+    TaskView(
+        task: TaskModel(
+            title: "Teste",
+            date: Date(),
+            isDone: false
+        )
+    )
 }
